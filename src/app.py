9cc309024f9357130d18bce08970c4d446118498
Jaristilde <154548762+Jaristilde@users.noreply.github.com>
2024-04-21 02:30:11 +0000

    April 17th Done

--- src/app.py
+++ src/app.py
@@ -1,27 +1,46 @@
-from flask import Flask, request, render_template
-from pickle import load
+import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestRegressor
 
-app = Flask(__name__)
-model = load(open("/workspaces/flask-render-integration/models/decision_tree_classifier_default_42.sav", "rb"))
-class_dict = {
-    "0": "Iris setosa",
-    "1": "Iris versicolor",
-    "2": "Iris virginica"
-}
+# Load the dataset (assuming it's in the same directory as your Streamlit app)
+total_data = pd.read_csv("../data/raw/House_Rent_Dataset.csv")
 
-@app.route("/", methods = ["GET", "POST"])
-def index():
-    if request.method == "POST":
-        
-        val1 = float(request.form["val1"])
-        val2 = float(request.form["val2"])
-        val3 = float(request.form["val3"])
-        val4 = float(request.form["val4"])
-        
-        data = [[val1, val2, val3, val4]]
-        prediction = str(model.predict(data)[0])
-        pred_class = class_dict[prediction]
-    else:
-        pred_class = None
-    
-    return render_template("index.html", prediction = pred_class)
+# Extract unique city names from the 'City' column
+unique_cities = total_data['City'].unique()
+
+# In your Streamlit app, or wherever you're making new predictions
+preprocessor = joblib.load('preprocessor.pkl')
+model = joblib.load('model.pkl')
+
+
+# Define a function to get user input
+def get_user_input():
+    city = st.sidebar.selectbox("City", options=unique_cities)
+    size = st.sidebar.number_input("Size", value=100.0, step=10.0, format="%.2f")
+    bathroom = st.sidebar.number_input("Number of Bathrooms", value=1, step=1)
+    return pd.DataFrame([[city, size, bathroom]], 
+                         columns=['City', 'Size', 'Bathroom'])
+
+
+# Set up the main app
+st.title('House Rent Prediction App')
+st.write('Enter the details of the house to predict the rent')
+
+
+# Get user input
+user_input = get_user_input()
+
+
+# Display the user input
+st.subheader('Your Input:')
+st.write(user_input)
+
+
+# Predict and display the output
+if st.button('Predict'):
+   #prediction = model.predict(user_input)
+   new_data_transformed = preprocessor.transform(user_input)
+   prediction = model.predict(new_data_transformed)
+   st.subheader('Predicted Monthly Rent')
+   st.write(f"${prediction[0]:,.2f}")
